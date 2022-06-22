1b5d44f7b73b62fc98319bc64c3ecc9e86dbc2a2
trent3134 <trentconway@yahoo.com>
2022-06-21 20:48:06 -0400

    Files

--- NBA.Data/ApplicationDbContext.cs
+++ NBA.Data/ApplicationDbContext.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
-namespace NBA.Data
-{
-    public class ApplicationDbContext
+public class ApplicationDbContext : DbContext
     {
-        
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {}
+
+        public DbSet<PlayersEntity> Players { get; set; }
+        public DbSet<TeamEntity> Teams { get; set; }
+        public DbSet<StadiumEntity> Stadium { get; set; }
     }
-}
