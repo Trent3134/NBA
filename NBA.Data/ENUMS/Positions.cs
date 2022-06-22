76c1658d62f935f8d92b4cdb43201128ba4210f3
trent3134 <trentconway@yahoo.com>
2022-06-21 20:47:10 -0400

    fileSetUp

---
+++ NBA.Data/ENUMS/Positions.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+    public enum Positions
+    {
+        PointGuard = 1,
+        ShootingGuard, 
+        SmallForward, 
+        PowerForward, 
+        Center
+
+
+    }
