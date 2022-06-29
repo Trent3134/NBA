3de968b5854cd4cce16a94de597136dd979f6b4d
Tracy Redman <redmanatracy@gmail.com>
2022-06-28 21:20:49 -0400

    TeamService

--- NBA.Services/TeamServices/ITeamService.cs
+++ NBA.Services/TeamServices/ITeamService.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
     {
         Task<bool> CreateTeamAsync(TeamCreate req);
         Task<IEnumerable<TeamListItem>> GetTeamByTeamName(string TeamName);
-        Task<IEnumerable<TeamListItem>> GetTeamByOwnerAsync(string TeamOwner);
+        Task<IEnumerable<TeamListItem>> GetTeamByTeamOwnerAsync(string TeamOwner);
         Task<IEnumerable<TeamListItem>> GetTeamByLocation(Locations Location);
         Task<IEnumerable<TeamListItem>> GetAllTeamsAsync();
         Task<bool> UpdateTeamAsync(TeamUpdate req);
--- NBA.Services/TeamServices/TeamService.cs
+++ NBA.Services/TeamServices/TeamService.cs
@@ -22,35 +22,45 @@ public class TeamService : ITeamService
         var numberOfChanges = await _context.SaveChangesAsync();
         return numberOfChanges == 1;
     }
-    
-    public async Task<TeamEntity> GetTeamByTeamName(string teamName)
+
+    public async Task<IEnumerable<PlayerListItem>> GetPlayersByTeam(int teamId)
     {
-        var team = await _context.Teams.Where(team => team.TeamName == teamName).ToListAsync();
+        var teamPlayer = await _context.Players.Where(p => p.TeamEntityId == teamId).ToListAsync();
+        if (teamPlayer is null)
+        {
+            return null;
+        }
+        return _mapper.Map<List<PlayerListItem>>(teamPlayer);
+    }
+    
+    public async Task<IEnumerable<TeamListItem>> GetTeamByTeamName(string TeamName)
+    {
+        var team = await _context.Teams.Where(t => t.TeamName == TeamName).ToListAsync();
         if (team is null)
         {
             return null;
         }
-        return _mapper.Map<TeamEntity>(team);
+        return _mapper.Map<List<TeamListItem>>(team);
     }
 
-    public async Task<IEnumerable<TeamDetail>> GetTeamByLocation(Locations locations)
+    public async Task<IEnumerable<TeamListItem>> GetTeamByLocation(Locations Location)
     {
-        var teams = await _context.Teams.Where(t => t.Locations == locations).ToListAsync();
+        var teams = await _context.Teams.Where(t => t.Locations == Location).ToListAsync();
         if (teams is null)
         {
             return null;
         }
-        return _mapper.Map<List<TeamDetail>>(teams);
+        return _mapper.Map<List<TeamListItem>>(teams);
     }
 
-    public async Task<IEnumerable<TeamListItem>> GetTeamByOwnerAsync(string TeamOwner)
+    public async Task<IEnumerable<TeamListItem>> GetTeamByTeamOwnerAsync(string TeamOwner)
     {
-        var teamOwner = await _context.Teams.Where(team => team.TeamOwner == TeamOwner).ToListAsync();
-        if (teamOwner is null)
+        var team = await _context.Teams.Where(t => t.TeamOwner == TeamOwner).ToListAsync();
+        if (team is null)
         {
             return null;
         }
-        return _mapper.Map<List<TeamListItem>>(teamOwner);
+        return _mapper.Map<List<TeamListItem>>(team);
     }
     public async Task<IEnumerable<TeamListItem>> GetAllTeamsAsync()
     {
